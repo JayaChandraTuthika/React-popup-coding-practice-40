57c3b7ac862231bd1163953f75690ebccf6d9083
JayaChandraTuthika <chandra4204@gmail.com>
2023-02-17 08:49:35 +0000

    submitted

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+import './index.css'
+
+const Home = () => (
+  <div className="bg-container">
+    <Header />
+
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+      alt="home"
+      className="section-image"
+    />
+  </div>
+)
+
+export default Home
